5e4b57a2e87a7dfc508b704dff586780902fd29a
Otacon <orfeo18@gmail.com>
2009-09-24 13:36:21 +0200

    Fixed some comments in PapyCache

--- mesinyer/e3/papylib/PapyCache.py
+++ mesinyer/e3/papylib/PapyCache.py
@@ -72,7 +72,8 @@ class PapyCache:
 
     def get(self, msn_object):
         '''Checks if msn_object is in cache. If it's not in cache
-        downloads it, then calls callback with the file-path as parameter.
+        downloads it and returns the path. If there's no support
+        in cache for this msnobject returns None.
         '''
         #Actually saves it in avatar cache...
         if msn_object._type == papyon.p2p.MSNObjectType.DYNAMIC_DISPLAY_PICTURE:
@@ -85,7 +86,6 @@ class PapyCache:
 
         elif msn_object._type == papyon.p2p.MSNObjectType.DISPLAY_PICTURE:
             if msn_object._data_sha not in self._avatar_cache:
-                print("Not In Cache!!!")
                 cbcks = (self._avatar_downloaded, self._download_failed)
                 self.msn_object_store.request(msn_object, cbcks)
             else:
@@ -104,8 +104,9 @@ class PapyCache:
 
     
     def _avatar_downloaded(self, msn_object, callback):
-        '''this callback saves the avatar, _msn_object_content is a callback
-        maybe the second invoked by _request_avatar with method "request"??'''
+        '''this callback saves the avatar in /tmp folder
+        then gives it to the caching system.
+        '''
         image = open( "/tmp/" + msn_object._data_sha,'w')
         image.write(msn_object._data.getvalue())
         image.flush()
@@ -113,8 +114,9 @@ class PapyCache:
         self._avatar_cache.insert(image.name)
 
     def _emoticon_downloaded(self, msn_object, callback):
-        '''this callback saves the emoticon, _msn_object_content is a callback
-        maybe the second invoked by _request_avatar with method "request"??'''
+        '''this callback saves the custom emoticon 
+        in /tmp folder then gives it to the caching system
+        '''
         image = open( "/tmp/"+msn_object._data_sha,'w')
         image.write(msn_object._data.getvalue())
         image.flush()
