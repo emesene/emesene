81c40accd159b79088c481e42e4c07242e573606
Adolfo Fitoria <fitoria@fitoria-laptop.(none)>
2010-02-19 23:06:45 -0600

    improving the amarok code quality (thanks pylint)

--- mesinyer/plugins/music/handler_amarok2.py
+++ mesinyer/plugins/music/handler_amarok2.py
@@ -1,5 +1,4 @@
 import songretriever
-import os
 
 class AmarokHandler(object):
     '''Handler for Amarok2'''
@@ -11,8 +10,8 @@ class AmarokHandler(object):
 
         try:
             import dbus
-            dbus_version = getattr(dbus, 'version', (0,0,0))
-            if dbus_version >=(0,41,0) and dbus_version < (0, 80, 0):
+            dbus_version = getattr(dbus, 'version', (0, 0, 0))
+            if dbus_version >= (0, 41, 0) and dbus_version < (0, 80, 0):
                 dbus.SessionBus()
                 import dbus.glib
             elif dbus_version >= (0, 80, 0):
@@ -22,21 +21,26 @@ class AmarokHandler(object):
             else:
                 print 'python-dbus is too old, please update'
                 raise
-        except Exception, e:
+        except:
             print 'cannot start dbus, please check if dbus is installed'
+        self.bus = None #dbus session, this is set in reconnect.
+        self.iface = None #dbus interface set in reconnect.
         self.module = dbus 
         self.reconnect()
 
     def reconnect(self):
+        '''method to attemp a reconnection, via dbus, this is only 
+        called if the bus object is not initialized'''
         self.bus = self.module.SessionBus()
         try:
-            self.iface= self.bus.get_object(self.iface_name, self.iface_path)
+            self.iface = self.bus.get_object(self.iface_name, self.iface_path)
             return True
         except:
-            self.iface=None
+            self.iface = None
             return False
     
     def is_running(self):
+        '''Returns a True if the player is running'''
         try:
             is_running_iface = self.bus.get_object(self.iface_name, '/Player')
             if is_running_iface:
@@ -44,18 +48,20 @@ class AmarokHandler(object):
             else: 
                 return False
         except:
-           return self.reconnect()
+            return self.reconnect()
     
     def is_playing(self):
+        '''Returns True if a song is being played'''
         if self.is_running():
             is_playing_iface = self.bus.get_object(self.iface_name, '/Player')
             if is_playing_iface:
                 status = is_playing_iface.GetStatus()
-                if status[0]==0:
+                if status[0] == 0:
                     return True
         return False
 
     def get_current_song(self):
+        '''Returns the current song in the correct format'''
         if self.is_playing():
             track = self.iface.GetCurrentTrack()
             song = self.iface.GetMetadata(track)
@@ -63,4 +69,5 @@ class AmarokHandler(object):
                                       song['album'],
                                       song['title'],
                                       song['location'])
+
 songretriever.register('Amarok2', AmarokHandler())
