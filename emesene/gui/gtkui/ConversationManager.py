1e032789e0aee1adadc64a63737d61675cc25542
hammered <hammered999@gmail.com>
2010-06-07 18:52:06 +0300

    Some indentation fixes on previous commit.

--- emesene/gui/gtkui/ConversationManager.py
+++ emesene/gui/gtkui/ConversationManager.py
@@ -130,15 +130,15 @@ class ConversationManager(gtk.Notebook, gui.ConversationManager):
         glib.idle_add(self._on_switch_page_grab_focus)
         
     def _on_switch_page_grab_focus(self):
-	'''Dirty hack. When _on_switch_page() is called the actual switch
+        '''Dirty hack. When _on_switch_page() is called the actual switch
 	    has not yet occured. We connect an idle handler to grab the focus,
 	    otherwise it won't work. Maybe this is solved if we iterate the
 	    mainloop inside of _on_switch_page() instead of connecting an
 	    idle signal.'''
-	page_num = self.get_current_page()
-	page = self.get_nth_page(page_num)
-	page.input.grab_focus()
-	return False
+        page_num = self.get_current_page()
+        page = self.get_nth_page(page_num)
+        page.input.grab_focus()
+        return False
 
     def _on_tab_close(self, button, conversation):
         '''called when the user clicks the close button on a tab'''
