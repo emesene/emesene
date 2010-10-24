b1ee9b5463044562bce82016ef29709910f12811
Mariano Guerra <luismarianoguerra@gmail.com>
2010-10-24 20:38:04 -0300

    changes to make emesene 2 work on windows under py2exe

--- emesene/e3/common/utils.py
+++ emesene/e3/common/utils.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import MessageFormatter
 
 def add_style_to_message(text, stl, escape=True):
@@ -33,3 +36,21 @@ def add_style_to_message(text, stl, escape=True):
 
     return style_start + text + style_end
 
+def we_are_frozen():
+    """Returns whether we are frozen via py2exe.
+    This will affect how we find out where we are located."""
+
+    return hasattr(sys, "frozen")
+
+def project_path():
+    """ This will get us the program's directory,
+    even if we are frozen using py2exe"""
+
+    if we_are_frozen():
+        return os.path.abspath(os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding())))
+
+
+    this_module_path = os.path.dirname(unicode(__file__, sys.getfilesystemencoding()))
+    proj_path = os.path.join(this_module_path, "..", "..")
+
+    return os.path.abspath(proj_path)
--- emesene/emesene.py
+++ emesene/emesene.py
@@ -20,8 +20,10 @@
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import os
+from e3.common.utils import project_path
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.abspath(project_path()))
+print (project_path())
 
 import sys
 import glib
