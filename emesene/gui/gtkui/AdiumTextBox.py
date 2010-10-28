d9c2ab912a4374abad006b1383bb7ca480eb8a62
Riccardo (C10uD) <c10ud.dev@gmail.com>
2010-10-28 18:46:39 +0200

    Fixed small font in AdiumTextBox, maybe it's an hack, but it works nicely (we don't get font size from the server messages). Closes #118

--- emesene/gui/gtkui/AdiumTextBox.py
+++ emesene/gui/gtkui/AdiumTextBox.py
@@ -157,6 +157,8 @@ class OutputText(gtk.ScrolledWindow):
     def receive_message(self, formatter, contact, message, cedict, cedir, is_first):
         '''add a message to the widget'''
         msg = gui.Message.from_contact(contact, message.body, is_first, True)
+        # WARNING: this is a hack to keep out config from backend libraries
+        message.style.size = self.config.get_or_set("i_font_size", 10)
         self.view.add_message(msg, message.style, cedict, cedir)
 
     def information(self, formatter, contact, message):
