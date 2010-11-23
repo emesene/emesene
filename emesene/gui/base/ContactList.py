725c681616a7aafc8aadad3056f1dc3fd8e928cb
Riccardo (C10uD) <c10ud.dev@gmail.com>
2010-11-23 19:32:32 +0100

    GPL header added to gui.base files

--- emesene/gui/base/ContactList.py
+++ emesene/gui/base/ContactList.py
@@ -1,6 +1,7 @@
+'''a abstract object that define the API of a contact list and some behavior'''
 # -*- coding: utf-8 -*-
 
-#   This file is part of emesene.
+#    This file is part of emesene.
 #
 #    emesene is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -15,7 +16,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with emesene; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-'''a abstract object that define the API of a contact list and some behavior'''
 
 import e3
 
--- emesene/gui/base/PictureHandler.py
+++ emesene/gui/base/PictureHandler.py
@@ -1,6 +1,8 @@
+'''This module contains the abstract class "PictureHandler", intended to save 
+and resize images.'''
 # -*- coding: utf-8 -*-
 
-#   This file is part of emesene.
+#    This file is part of emesene.
 #
 #    emesene is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -15,9 +17,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with emesene; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-'''This module contains the abstract class "PictureHandler", intended to save 
-and resize images.'''
 
 import shutil
 
