f09ac0f1807fcaa4cb0255d4bc2363721a3b3816
Riccardo (C10uD) <c10ud.dev@gmail.com>
2010-11-23 19:29:53 +0100

    GPL header added to gtkui files

--- emesene/gui/gtkui/Parser.py
+++ emesene/gui/gtkui/Parser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#   This file is part of emesene.
+#    This file is part of emesene.
 #
 #    emesene is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
--- emesene/gui/gtkui/Renderers.py
+++ emesene/gui/gtkui/Renderers.py
@@ -1,7 +1,7 @@
 '''renderers for the ContactList'''
 # -*- coding: utf-8 -*-
 
-#   This file is part of emesene.
+#    This file is part of emesene.
 #
 #    emesene is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
