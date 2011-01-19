9c3a50ec4a9fcaa618e5cc723ea2e253582b052c
4ndreaSt4gi <stagi.andrea@gmail.com>
2011-01-19 18:46:54 +0100

    Notification when file arrives or transfer fails. Theme path modified in absolute path. Image preview resized

--- emesene/gui/gtkui/FileTransferWidget.py
+++ emesene/gui/gtkui/FileTransferWidget.py
@@ -20,6 +20,7 @@
 import gtk
 import pango
 import gobject
+import extension
 
 import e3.base
 import gui
@@ -68,6 +69,8 @@ class FileTransferWidget(gtk.HBox):
         self.show_all()
         self.tooltip = FileTransferTooltip(self.event_box, self.transfer)
 
+        self.notifier = extension.get_default('notificationGUI')
+
         self.event_box.connect('event', self._on_progressbar_event)
 
         self.do_update_progress()
@@ -115,13 +118,18 @@ class FileTransferWidget(gtk.HBox):
 
         self.buttons = []
 
-        if state == self.transfer.WAITING and self.transfer.sender != 'Me':
+        if state == self.transfer.WAITING and self.transfer.sender != 'Me':        
             button = gtk.Button(None, None)
             button.set_image(self.__get_button_img(gtk.STOCK_APPLY))
             button.connect('clicked', self._on_accept_clicked)
             self.buttons.append(button)
 
         if state in (self.transfer.RECEIVED, self.transfer.FAILED):
+            if(state==self.transfer.RECEIVED):
+                self.notifier("File transfer successful", "", "file://" + gui.theme.transfer_success)   
+            elif(state==self.transfer.FAILED):
+                self.notifier("File transfer failed", "", gtk.STOCK_CANCEL)
+
             button = gtk.Button(None, None)
             button.set_image(self.__get_button_img(gtk.STOCK_CLEAR))
             button.connect('clicked', self._on_close_clicked)
@@ -241,8 +249,12 @@ class FileTransferTooltip(gtk.Window):
         else:
             pixbuf = None
         #amsn sends a big. black preview? :S
-        if pixbuf and pixbuf.get_height() <= 96 and pixbuf.get_width() <= 96:
-            self.image.set_from_pixbuf(pixbuf)
+        if pixbuf:
+            if pixbuf.get_height() <= 96 and pixbuf.get_width() <= 96:
+                self.image.set_from_pixbuf(pixbuf)
+            else:
+                pixbuf.scale_simple(96, 96, gtk.gdk.INTERP_BILINEAR)
+                self.image.set_from_pixbuf(pixbuf)
 
         # set the location of the tooltip
         x, y = self.find_position(o_coords, view.window)
