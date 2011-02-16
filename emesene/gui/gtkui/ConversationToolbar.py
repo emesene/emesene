f3c03e108db70411c596957b14d34dc46cd9e28c
Sven (Sbte) <svenb.linux@gmail.com>
2011-02-16 20:19:45 +0100

    Fix the indentation in ConversationToolbar

--- emesene/gui/gtkui/ConversationToolbar.py
+++ emesene/gui/gtkui/ConversationToolbar.py
@@ -42,12 +42,10 @@ class ConversationToolbar(gtk.Toolbar):
 
         self.handler = handler
 
-
         self.draw()
 
-
     def draw(self):
-
+        '''draw the toolbar'''
         toolbar_small = self.handler.session.config.get_or_set('b_toolbar_small', False)
 
         if toolbar_small:
@@ -63,113 +61,113 @@ class ConversationToolbar(gtk.Toolbar):
 
         # check if we have theme-specific toolbar-icons
 
-	if gui.theme.toolbar_path:
-	    theme_tool_font = utils.gtk_ico_image_load(gui.theme.tool_font, size)
-	    theme_tool_font_color = utils.gtk_ico_image_load(gui.theme.tool_font_color, size)
-	    theme_tool_emotes = utils.gtk_ico_image_load(gui.theme.tool_emotes, size)
-	    theme_tool_nudge = utils.gtk_ico_image_load(gui.theme.tool_nudge, size)
-	    theme_tool_invite = utils.gtk_ico_image_load(gui.theme.tool_invite, size)
-	    theme_tool_clean = utils.gtk_ico_image_load(gui.theme.tool_clean, size)
-	    theme_tool_file_transfer = utils.gtk_ico_image_load(gui.theme.tool_file_transfer, size)
-	    theme_tool_ublock = utils.gtk_ico_image_load(gui.theme.tool_ublock, size)
-	else:
-	    theme_tool_font = gtk.STOCK_SELECT_FONT
-	    theme_tool_font_color = gtk.STOCK_SELECT_COLOR
-	    theme_tool_emotes = utils.gtk_ico_image_load(gui.theme.emote_to_path(':D', True), size)
-	    theme_tool_nudge = utils.gtk_ico_image_load(gui.theme.emote_to_path(':S', True), size)
-	    theme_tool_invite = gtk.STOCK_ADD
-	    theme_tool_clean = gtk.STOCK_CLEAR
-	    theme_tool_file_transfer = gtk.STOCK_GO_UP
-	    theme_tool_ublock = gtk.STOCK_STOP
+        if gui.theme.toolbar_path:
+            theme_tool_font = utils.gtk_ico_image_load(gui.theme.tool_font, size)
+            theme_tool_font_color = utils.gtk_ico_image_load(gui.theme.tool_font_color, size)
+            theme_tool_emotes = utils.gtk_ico_image_load(gui.theme.tool_emotes, size)
+            theme_tool_nudge = utils.gtk_ico_image_load(gui.theme.tool_nudge, size)
+            theme_tool_invite = utils.gtk_ico_image_load(gui.theme.tool_invite, size)
+            theme_tool_clean = utils.gtk_ico_image_load(gui.theme.tool_clean, size)
+            theme_tool_file_transfer = utils.gtk_ico_image_load(gui.theme.tool_file_transfer, size)
+            theme_tool_ublock = utils.gtk_ico_image_load(gui.theme.tool_ublock, size)
+        else:
+            theme_tool_font = gtk.STOCK_SELECT_FONT
+            theme_tool_font_color = gtk.STOCK_SELECT_COLOR
+            theme_tool_emotes = utils.gtk_ico_image_load(gui.theme.emote_to_path(':D', True), size)
+            theme_tool_nudge = utils.gtk_ico_image_load(gui.theme.emote_to_path(':S', True), size)
+            theme_tool_invite = gtk.STOCK_ADD
+            theme_tool_clean = gtk.STOCK_CLEAR
+            theme_tool_file_transfer = gtk.STOCK_GO_UP
+            theme_tool_ublock = gtk.STOCK_STOP
 
-	theme_tool_call = utils.gtk_ico_image_load(gui.theme.call, size)
-	theme_tool_video = utils.gtk_ico_image_load(gui.theme.video, size)
-	theme_tool_av = utils.gtk_ico_image_load(gui.theme.av, size)
+        theme_tool_call = utils.gtk_ico_image_load(gui.theme.call, size)
+        theme_tool_video = utils.gtk_ico_image_load(gui.theme.video, size)
+        theme_tool_av = utils.gtk_ico_image_load(gui.theme.av, size)
 
-	self.font = gtk.ToolButton(theme_tool_font)
-	self.font.set_label(_('Select font'))
-	self.font.set_tooltip_text(_('Select font')) 
-	self.font.connect('clicked',
-	    lambda *args: self.handler.on_font_selected())
+        self.font = gtk.ToolButton(theme_tool_font)
+        self.font.set_label(_('Select font'))
+        self.font.set_tooltip_text(_('Select font')) 
+        self.font.connect('clicked',
+            lambda *args: self.handler.on_font_selected())
 
-	self.color = gtk.ToolButton(theme_tool_font_color)
-	self.color.set_label(_('Select font color'))
-	self.color.set_tooltip_text(_('Select font color')) 
-	self.color.connect('clicked',
-	    lambda *args: self.handler.on_color_selected())
+        self.color = gtk.ToolButton(theme_tool_font_color)
+        self.color.set_label(_('Select font color'))
+        self.color.set_tooltip_text(_('Select font color')) 
+        self.color.connect('clicked',
+            lambda *args: self.handler.on_color_selected())
 
-	self.emotes = gtk.ToolButton(theme_tool_emotes)
-	self.emotes.set_label(_('Send an emoticon'))
-	self.emotes.set_tooltip_text(_('Send an emoticon')) 
-	self.emotes.connect('clicked',
-	    lambda *args: self.handler.on_emotes_selected())
+        self.emotes = gtk.ToolButton(theme_tool_emotes)
+        self.emotes.set_label(_('Send an emoticon'))
+        self.emotes.set_tooltip_text(_('Send an emoticon')) 
+        self.emotes.connect('clicked',
+            lambda *args: self.handler.on_emotes_selected())
 
-	self.nudge = gtk.ToolButton(theme_tool_nudge)
-	self.nudge.set_label(_('Request attention'))
-	self.nudge.set_tooltip_text(_('Request attention')) 
-	self.nudge.connect('clicked',
-	    lambda *args: self.handler.on_notify_attention_selected())
+        self.nudge = gtk.ToolButton(theme_tool_nudge)
+        self.nudge.set_label(_('Request attention'))
+        self.nudge.set_tooltip_text(_('Request attention')) 
+        self.nudge.connect('clicked',
+            lambda *args: self.handler.on_notify_attention_selected())
 
-	self.invite = gtk.ToolButton(theme_tool_invite)
-	self.invite.set_label(_('Invite a buddy'))
-	self.invite.set_tooltip_text(_('Invite a buddy')) 
-	self.invite.connect('clicked',
-	    lambda *args: self.handler.on_invite_selected())
+        self.invite = gtk.ToolButton(theme_tool_invite)
+        self.invite.set_label(_('Invite a buddy'))
+        self.invite.set_tooltip_text(_('Invite a buddy')) 
+        self.invite.connect('clicked',
+            lambda *args: self.handler.on_invite_selected())
 
-	self.clean = gtk.ToolButton(theme_tool_clean)
-	self.clean.set_label(_('Clean the conversation'))
-	self.clean.set_tooltip_text(_('Clean the conversation')) 
-	self.clean.connect('clicked',
-	    lambda *args: self.handler.on_clean_selected())
+        self.clean = gtk.ToolButton(theme_tool_clean)
+        self.clean.set_label(_('Clean the conversation'))
+        self.clean.set_tooltip_text(_('Clean the conversation')) 
+        self.clean.connect('clicked',
+            lambda *args: self.handler.on_clean_selected())
 
-	self.invite_file_transfer = gtk.ToolButton(theme_tool_file_transfer)
-	self.invite_file_transfer.set_label(_('Send a file'))
-	self.invite_file_transfer.set_tooltip_text(_('Send a file')) 
-	self.invite_file_transfer.connect('clicked',
-	    lambda *args: self.handler.on_invite_file_transfer_selected())
+        self.invite_file_transfer = gtk.ToolButton(theme_tool_file_transfer)
+        self.invite_file_transfer.set_label(_('Send a file'))
+        self.invite_file_transfer.set_tooltip_text(_('Send a file')) 
+        self.invite_file_transfer.connect('clicked',
+            lambda *args: self.handler.on_invite_file_transfer_selected())
 
-	self.ublock = gtk.ToolButton(theme_tool_ublock)
-	self.ublock.set_label(_('Block/Unblock contact'))
-	self.ublock.set_tooltip_text(_('Block/Unblock contact')) 
-	self.ublock.connect('clicked',
-	    lambda *args: self.handler.on_ublock_selected())
+        self.ublock = gtk.ToolButton(theme_tool_ublock)
+        self.ublock.set_label(_('Block/Unblock contact'))
+        self.ublock.set_tooltip_text(_('Block/Unblock contact')) 
+        self.ublock.connect('clicked',
+            lambda *args: self.handler.on_ublock_selected())
 
-	self.invite_video_call = gtk.ToolButton(theme_tool_video)
-	self.invite_video_call.set_label(_('Video Call'))
-	self.invite_video_call.set_tooltip_text(_('Video Call')) 
-	self.invite_video_call.connect('clicked',
-	    lambda *args: self.handler.on_invite_video_call_selected())
+        self.invite_video_call = gtk.ToolButton(theme_tool_video)
+        self.invite_video_call.set_label(_('Video Call'))
+        self.invite_video_call.set_tooltip_text(_('Video Call')) 
+        self.invite_video_call.connect('clicked',
+            lambda *args: self.handler.on_invite_video_call_selected())
 
-	self.invite_audio_call = gtk.ToolButton(theme_tool_call)
-	self.invite_audio_call.set_label(_('Voice Call'))
-	self.invite_audio_call.set_tooltip_text(_('Voice Call')) 
-	self.invite_audio_call.connect('clicked',
-	    lambda *args: self.handler.on_invite_voice_call_selected())
+        self.invite_audio_call = gtk.ToolButton(theme_tool_call)
+        self.invite_audio_call.set_label(_('Voice Call'))
+        self.invite_audio_call.set_tooltip_text(_('Voice Call')) 
+        self.invite_audio_call.connect('clicked',
+            lambda *args: self.handler.on_invite_voice_call_selected())
 
-	self.invite_av_call = gtk.ToolButton(theme_tool_av)
-	self.invite_av_call.set_label(_('Audio/Video Call'))
-	self.invite_av_call.set_tooltip_text(_('Audio/Video Call')) 
-	self.invite_av_call.connect('clicked',
-	    lambda *args: self.handler.on_invite_av_call_selected())
+        self.invite_av_call = gtk.ToolButton(theme_tool_av)
+        self.invite_av_call.set_label(_('Audio/Video Call'))
+        self.invite_av_call.set_tooltip_text(_('Audio/Video Call')) 
+        self.invite_av_call.connect('clicked',
+            lambda *args: self.handler.on_invite_av_call_selected())
 
-	self.add(self.font)
-	self.add(self.color)
-	self.add(gtk.SeparatorToolItem())
+        self.add(self.font)
+        self.add(self.color)
+        self.add(gtk.SeparatorToolItem())
 
-	self.add(self.emotes)
-	self.add(self.nudge)
-	self.add(gtk.SeparatorToolItem())
+        self.add(self.emotes)
+        self.add(self.nudge)
+        self.add(gtk.SeparatorToolItem())
 
-	self.add(self.invite)
-	self.add(self.invite_file_transfer)
-	self.add(gtk.SeparatorToolItem())
+        self.add(self.invite)
+        self.add(self.invite_file_transfer)
+        self.add(gtk.SeparatorToolItem())
 
-	#self.add(self.invite_video_call)
-	#self.add(self.invite_audio_call)
-	#self.add(self.invite_av_call)
-	#self.add(gtk.SeparatorToolItem())
+        #self.add(self.invite_video_call)
+        #self.add(self.invite_audio_call)
+        #self.add(self.invite_av_call)
+        #self.add(gtk.SeparatorToolItem())
 
-	self.add(self.clean)
-	self.add(self.ublock)
-	self.add(gtk.SeparatorToolItem())
+        self.add(self.clean)
+        self.add(self.ublock)
+        self.add(gtk.SeparatorToolItem())
 
