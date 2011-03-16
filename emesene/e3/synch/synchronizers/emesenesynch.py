cf62137f176afe13a39114500f153c1f78bbbe8e
4ndreaSt4gi <stagi.andrea@gmail.com>
2011-03-16 20:03:18 +0100

    Licence added

--- emesene/e3/synch/synchronizers/emesenesynch.py
+++ emesene/e3/synch/synchronizers/emesenesynch.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#   This file is part of emesene.
+#
+#    Emesene is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    emesene is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with emesene; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#    Module written by Andrea Stagi <stagi.andrea(at)gmail.com>
+
 from synch import synch
 import sqlite3.dbapi2 as sqlite
 from datetime import date
