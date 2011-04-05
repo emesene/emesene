9335753e8617c243f5b7e3954efd56b49afd7d71
Riccardo (C10uD) <c10ud.dev@gmail.com>
2011-04-05 18:40:53 +0200

    Proper license box in about window

--- emesene/gui/base/Handler.py
+++ emesene/gui/base/Handler.py
@@ -28,6 +28,21 @@ import Desktop
 import logging
 log = logging.getLogger('gui.base.Handler')
 
+EMESENE_LICENSE = '''    emesene is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    emesene is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with emesene; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+'''
+
 class MenuHandler(object):
     '''this handler contains all the handlers needed to handle all the
     menu items
@@ -144,7 +159,7 @@ class HelpHandler(object):
     def on_about_selected(self):
         '''called when the about item is selected'''
         self.dialog.about_dialog('emesene', '2.0', 'marianoguerra & c10ud',
-            _('A simple yet powerful instant messaging client'), 'GPL v3',
+            _('A simple yet powerful instant messaging client'), EMESENE_LICENSE,
             'http://www.emesene.org', 
             [ 'Riccardo (c10ud) <c10ud.dev@gmail.com>',
               'Mariano Guerra <luismarianoguerra@gmail.com>',
