ed7afda5781fdb4bde9a1b84921f7d9bb37107af
Riccardo (C10uD) <c10ud.dev@gmail.com>
2011-05-02 18:54:03 +0200

    forgot import os

--- emesene/gui/gtkui/TabWidget.py
+++ emesene/gui/gtkui/TabWidget.py
@@ -16,6 +16,7 @@
 #    along with emesene; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import os
 import gtk
 import gobject
 
