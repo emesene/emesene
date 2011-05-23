emesene/emesene

commit f4db1da058ee94f180b94f9730e458d2babaa778

Add a tooltip with contact's email on GtkNotifications

emesene/gui/gtkui/PyNotification.py
@@ -29,7 +29,7 @@ DESCRIPTION = 'Wrapper around pynotify for the notification system'
 AUTHOR = 'arielj'
 WEBSITE = 'www.emesene.org'
 
-def pyNotification(title, text, picturePath=None,const=None):
+def pyNotification(title, text, picturePath=None, const=None, callback=None, tootltip=None):
     if (const=='message-im'):
         #In this case title is contact nick
         title = Renderers.msnplus_to_plain_text(title)

emesene/e3/common/notification.py
@@ -75,12 +75,12 @@ class Notification():
             return
 
         contact = self.session.contacts.get(arg1.sender.account)
-        self._notify(contact, contact.nick, _("File transfer invitation"))
+        self._notify(contact, contact.nick, _("File transfer invitation"), arg1.sender.account)
         
     def _on_mail_received(self, message):
         ''' called when a new mail is received '''
-        self.notifier(_("New mail from %s") % (message.address), message._subject, 'notification-message-email','mail-received')
-
+        self.notifier(_("New mail from %s") % (message.address), message._subject, 'notification-message-email','mail-received', None, message.address)
+ 
     def has_similar_conversation(self, cid, members):
         '''
         try to find a conversation with the given cid, if not search for a
@@ -115,7 +115,7 @@ class Notification():
             else:
                 text = msgobj.body
 
-        self._notify(contact, contact.nick , text, None)
+        self._notify(contact, contact.nick , text, contact.account, None)
 
     def _on_contact_attr_changed(self, account, change_type, old_value,
             do_notify=True):
@@ -137,7 +137,7 @@ class Notification():
                     text = _('is online')
                 if self.session.config.b_play_contact_online:
                     sound = gui.theme.sound_online
-                self._notify(contact, contact.nick, text, sound)
+                self._notify(contact, contact.nick, text, contact.account, sound)
 
             if not self.notify_online:
                 # detects the first notification flood and enable the
@@ -160,9 +160,9 @@ class Notification():
             if self.session.config.b_play_contact_offline:
                 sound = gui.theme.sound_offline
 
-            self._notify(contact, contact.nick, text, sound)
+            self._notify(contact, contact.nick, text, contact.account, sound)
 
-    def _notify(self, contact, title, text, sound=None):
+    def _notify(self, contact, title, text, tooltip, sound=None):
         """
         This creates and shows the nofification
         """
@@ -172,6 +172,6 @@ class Notification():
             uri = 'notification-message-im'
 
         if text is not None:
-            self.notifier(title, text, uri,'message-im')
+            self.notifier(title, text, uri, 'message-im', None, tooltip)
         if sound is not None:
             self.sound_player.play(sound)

emesene/gui/gtkui/GtkNotification.py
@@ -110,7 +110,7 @@ if os.name == "nt":
 queue = list()
 actual_notification = None
 
-def gtkNotification(title, text, picturePath=None, const=None, callback=None):
+def gtkNotification(title, text, picturePath=None, const=None, callback=None, tooltip=None):
     global actual_notification
     global queue
 
@@ -122,7 +122,7 @@ def gtkNotification(title, text, picturePath=None, const=None, callback=None):
         title = Renderers.msnplus_to_plain_text(title)
 
     if actual_notification is None:
-        actual_notification = Notification(title, text, picturePath, callback)
+        actual_notification = Notification(title, text, picturePath, callback, tooltip)
         actual_notification.show()
     else:
         # Append text to the actual notification
@@ -131,11 +131,11 @@ def gtkNotification(title, text, picturePath=None, const=None, callback=None):
         else:
             found = False
             auxqueue = list()
-            for _title, _text, _picturePath, _callback in queue:
+            for _title, _text, _picturePath, _callback, _tooltip in queue:
                 if _title == title:
                     _text = _text + "\n" + text
                     found = True
-                auxqueue.append([_title,_text,_picturePath, _callback])
+                auxqueue.append([_title,_text,_picturePath, _callback, _tooltip])
 
             if found:
                 # append text to another notification
@@ -143,10 +143,10 @@ def gtkNotification(title, text, picturePath=None, const=None, callback=None):
                 queue = auxqueue
             else:
                 # or queue a new notification
-                queue.append([title, text, picturePath, callback])
+                queue.append([title, text, picturePath, callback, tooltip])
 
 class Notification(gtk.Window):
-    def __init__(self, title, text, picturePath, callback):
+    def __init__(self, title, text, picturePath, callback, tooltip):
 
         gtk.Window.__init__(self, type=gtk.WINDOW_POPUP)
 
@@ -199,6 +199,9 @@ class Notification(gtk.Window):
         lboxEventBox.connect("button_press_event", self.onClick)
         lboxEventBox.add(lbox)
         self.connect("button_press_event", self.onClick)
+
+        if tooltip is not None:
+            lboxEventBox.set_tooltip_text(tooltip)
 
         # pack everything
         self.messageVbox.pack_start(titleLabel, False, False)
@@ -306,9 +309,9 @@ class Notification(gtk.Window):
         if self.timerId is not None:
             glib.source_remove(self.timerId)
         if len(queue) != 0:
-            title, text, picturePath, callback = queue.pop(0)
+            title, text, picturePath, callback, tooltip = queue.pop(0)
             actual_notification = Notification(title, text, picturePath, \
-                                               callback)
+                                               callback, tooltip)
             actual_notification.show()
         else:
             actual_notification = None

commit 4ddc2a0a6c9915ff6f3afcb6e3aad2ebf6955d77

gtk: Fix ThemeNotification not accepting enough arguments. Also makes pylint a lot happier
Fixes a regression introduced by f4db1da058ee94f180b94f9730e458d2babaa778

emesene/gui/gtkui/PyNotification.py
@@ -29,10 +29,11 @@ DESCRIPTION = 'Wrapper around pynotify for the notification system'
 AUTHOR = 'arielj'
 WEBSITE = 'www.emesene.org'
 
-def pyNotification(title, text, picturePath=None, const=None, callback=None, tootltip=None):
-    if (const=='message-im'):
+def PyNotification(title, text, picture_path=None, const=None,
+                   callback=None, tooltip=None):
+    if const == 'message-im':
         #In this case title is contact nick
         title = Renderers.msnplus_to_plain_text(title)
-    n = pynotify.Notification(title, text, picturePath)
-    n.set_hint_string("append", "allowed")
-    n.show()
+    notification = pynotify.Notification(title, text, picture_path)
+    notification.set_hint_string("append", "allowed")
+    notification.show()

emesene/gui/gtkui/ThemeNotification.py
@@ -26,33 +26,35 @@ import logging
 log = logging.getLogger('gui.gtkui.ThemeNotification')
 
 NAME = 'ThemeNotification'
-DESCRIPTION = 'Wrapper around pynotify for the notification system. Notifier depends by the current theme'
+DESCRIPTION = 'Wrapper around pynotify for the notification system. \
+               Notifier depends by the current theme'
 AUTHOR = 'Andrea Stagi'
 WEBSITE = 'www.emesene.org'
 
-def themeNotification(title, text, picturePath=None,const=None):
-    
-    def pictureFactory(picture,constValue):
+def ThemeNotification(title, text, picture_path=None, const=None,
+                      callback=None, tooltip=None):
 
-        if(picture):
+    def picture_factory(picture, const_value):
+        ''' decides which theme picture to use '''
+
+        if picture:
             if(picture[:7]=="file://"):
                 return picture
-        if(constValue=='mail-received'):
+        if const_value == 'mail-received':
             return "file://" + gui.theme.email
-        elif(constValue=='file-transf-completed'):
+        elif const_value == 'file-transf-completed':
             return "file://" + gui.theme.transfer_success
-        elif(constValue=='file-transf-canceled'):
+        elif const_value == 'file-transf-canceled':
             return "file://" + gui.theme.transfer_unsuccess
-        elif(constValue=='message-im'):
+        elif const_value == 'message-im':
             return "file://" + gui.theme.user_def_imagetool
         else:
             return "file://" + gui.theme.user_def_imagetool
 
-    if (const=='message-im'):
+    if const == 'message-im':
         #In this case title is contact nick
         title = Renderers.msnplus_to_plain_text(title)
-    n = pynotify.Notification(title, text, pictureFactory(picturePath,const)) 
-    n.set_hint_string("append", "allowed")
-    n.show()
-
-
+    notification = pynotify.Notification(title, text,
+                            picture_factory(picture_path, const))
+    notification.set_hint_string("append", "allowed")
+    notification.show()

emesene/gui/gtkui/__init__.py
@@ -200,8 +200,8 @@ def setup():
         extension.category_register('conversation output', TextBox.OutputText)
 
     if not PYNOTIFYERROR:
-        extension.category_register(('notificationGUI'), ThemeNotification.themeNotification)
-        extension.register(('notificationGUI'), PyNotification.pyNotification)
+        extension.category_register(('notificationGUI'), ThemeNotification.ThemeNotification)
+        extension.register(('notificationGUI'), PyNotification.PyNotification)
         extension.register(('notificationGUI'), GtkNotification.gtkNotification)
     else: #leave this here for Windows users!
         extension.category_register(('notificationGUI'), GtkNotification.gtkNotification)