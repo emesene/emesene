4e6dd54285816914a02203917e06a28f19c2ed80
Jose Rostagno <joserostagno@vijona.com.ar>
2011-08-15 11:54:09 -0300

    qt4ui: update adium text output.

--- emesene/gui/qt4ui/widgets/AdiumChatOutput.py
+++ emesene/gui/qt4ui/widgets/AdiumChatOutput.py
@@ -10,7 +10,7 @@ from PyQt4      import QtWebKit
 
 import e3
 import gui
-
+from gui.base import Plus
 from gui.qt4ui  import Utils
 from gui.qt4ui.Utils import tr
 
@@ -30,6 +30,11 @@ class AdiumChatOutput (QtGui.QScrollArea):
     def __init__(self, parent=None):
         QtGui.QScrollArea.__init__(self, parent)
         
+        self.theme = gui.theme.conv_theme
+        self.last_incoming = None
+        self.last_incoming_account = None
+        self.last_incoming_nickname = None
+
         self._qwebview = QtWebKit.QWebView(self)
         self._last_sender = None
         
@@ -46,54 +51,61 @@ class AdiumChatOutput (QtGui.QScrollArea):
         self._qwebview.linkClicked.connect(
                         lambda qt_url: webbrowser.open(qt_url.toString()) )
                             
-                            
-    def _append_message(self, contact, message, cedict,
-                        cedir, is_incoming, timestamp=None):
-        
-        msg = gui.Message.from_contact(contact, message, first=True,
-                                       incomming=is_incoming, tstamp=timestamp)
-        if msg.sender != self._last_sender:
-            function = "appendMessage('%s')"
+    def _append_message(self, msg, style=None, cedict={}, cedir=None):
+        '''add a message to the conversation'''
+
+        b_nick_check = bool(self.last_incoming_nickname != msg.display_name)
+        if b_nick_check:
+            self.last_incoming_nickname = msg.display_name
+
+        if msg.incoming:
+            if self.last_incoming is None:
+                self.last_incoming = False
+
+            msg.first = not self.last_incoming
+
+            if self.last_incoming_account != msg.sender or b_nick_check:
+                msg.first = True
+
+            html = self.theme.format_incoming(msg, style, cedict, cedir)
+            self.last_incoming = True
+            self.last_incoming_account = msg.sender
+        else:
+            if self.last_incoming is None:
+                self.last_incoming = True
+
+            msg.first = self.last_incoming
+
+            html = self.theme.format_outgoing(msg, style, cedict, cedir)
+            self.last_incoming = False
+
+        if msg.type == "status":
+            self.last_incoming = None
             msg.first = True
-            self._last_sender = msg.sender
+
+        if msg.first:
+            function = "appendMessage('" + html + "')"
         else:
-            function = "appendNextMessage('%s')"
-            msg.first = False
-            
-        if is_incoming:
-            html = gui.theme.conv_theme.format_incoming(msg, message.style, 
-                                                        cedict, cedir)
-        else:
-            html = gui.theme.conv_theme.format_outgoing(msg, message.style, 
-                                                        cedict, cedir)
-            
-        self._qwebview.page().mainFrame().evaluateJavaScript(function % html)
-                            
+            function = "appendNextMessage('" + html + "')"
+
+        self._qwebview.page().mainFrame().evaluateJavaScript(function)
+
+    def send_message(self, formatter, contact, message, cedict, cedir, is_first):
+        '''add a message to the widget'''
+        msg = gui.Message.from_contact(contact, message, is_first, False, message.timestamp)
+        self._append_message(msg, message.style, cedict, cedir)
+
+    def receive_message(self, formatter, contact, message, cedict, cedir, is_first):
+        '''add a message to the widget'''
+        msg = gui.Message.from_contact(contact, message, is_first, True, message.timestamp)
+        self._append_message(msg, message.style, cedict, cedir)
 
     def information(self, formatter, contact, message):
         '''add an information message to the widget'''
-        # TODO: make it with a status message
-        self._append_message(contact, message, cedict={}, 
-                             cedir='', is_incoming=True)
-        
-        
-    def send_message(self, formatter, contact, message, 
-                     cedict, cedir, is_first):
-        '''add a message to the widget'''       
-        self._append_message(contact, message, cedict, 
-                             cedir, is_incoming=False)
-        
-        
-    def receive_message(self, formatter, contact, 
-                        message, cedict, cedir, is_first):
-        '''add a message to the widget'''
-        # WARNING: this is a hack to keep out config from backend libraries
-        #message.style.size = self.config.i_font_size
-        self._append_message(contact, message, cedict, cedir, 
-                             is_incoming=True, 
-                             timestamp=message.timestamp)
+        msg = gui.Message.from_information(contact, message)
+        msg.message = Plus.msnplus_strip(msg.message)
+        self._append_message(msg, None, None, None)
 
-        
     def update_p2p(self, account, _type, *what):
         ''' new p2p data has been received (custom emoticons) '''
         if _type == 'emoticon':
