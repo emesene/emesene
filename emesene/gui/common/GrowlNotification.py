31b1b5bc4c297e7ca37b3a9f83ef4f3f071837ee
Josh Fradley <josh.fradley@gmail.com>
2011-08-19 18:44:46 +0100

    Update growl plugin as emesene returns default avatar if no contact picture exists, removed the uneeded code

--- emesene/gui/common/GrowlNotification.py
+++ emesene/gui/common/GrowlNotification.py
@@ -31,14 +31,11 @@ NAME = 'GrowlNotification'
 DESCRIPTION = 'Wrapper around growlnotify for the notification system'
 AUTHOR = 'joshf'
 WEBSITE = 'www.sidhosting.co.uk'
-VERSION = '0.4'
+VERSION = '0.5'
 
 def GrowlNotification(title, text, uri, picture_path=None, const=None, 
                       callback=None, tooltip=None):
     title = Plus.msnplus_strip(title)
-    if uri == 'notification-message-im':
-        subprocess.call(['/usr/local/bin/growlnotify', '-n', 'emesene', '-a', 'emesene', '-t', title, '-m', text])
-    else:
-        imagepath = uri.replace( "file:///", "/" )
-        #BUG: In growl prefs, the emesene ticket will take on the last shown contact's picture
-        subprocess.call(['/usr/local/bin/growlnotify', '-n', 'emesene', '--image', imagepath, '-t', title, '-m', text])
+    imagepath = uri.replace( "file:///", "/" )
+    #BUG: In growl prefs, the emesene ticket will take on the last shown contact's picture
+    subprocess.call(['/usr/local/bin/growlnotify', '-n', 'emesene', '--image', imagepath, '-t', title, '-m', text])
