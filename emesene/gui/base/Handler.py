5a4f829189d36ff32580f7acca793af47166a790
Riccardo (C10uD) <c10ud.dev@gmail.com>
2011-09-29 23:09:36 +0200

    emesene: bump to version 2.11.10-devel
    now you can commit like crazy!

--- emesene/gui/base/Handler.py
+++ emesene/gui/base/Handler.py
@@ -46,7 +46,7 @@ EMESENE_LICENSE = '''    emesene is free software; you can redistribute it and/o
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
-EMESENE_VERSION = "2.11.9"
+EMESENE_VERSION = "2.11.10-devel"
 EMESENE_LAST_STABLE = "2.11.9"
 EMESENE_WEBSITE = "http://www.emesene.org"
 EMESENE_AUTHORS = "marianoguerra & c10ud"
