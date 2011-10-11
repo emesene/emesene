5dcff1a8e35d545d876fddc0b2a8960e369a5e09
Jose Rostagno <joserostagno@vijona.com.ar>
2011-10-11 19:30:20 -0300

    ConversationStatus: unused import

--- emesene/gui/base/ConversationStatus.py
+++ emesene/gui/base/ConversationStatus.py
@@ -18,7 +18,6 @@
 #    along with emesene; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import e3
 import gui
 import MarkupParser
 
