8b2939f2abdec722bc66d29088e5b1cbb9ba67aa
Riccardo (C10uD) <c10ud.dev@gmail.com>
2012-04-20 18:01:20 +0200

    papylib: handle weird content roaming IO errors. Fixes #1178 and LP#976615

--- emesene/e3/papylib/PapyEvents.py
+++ emesene/e3/papylib/PapyEvents.py
@@ -80,8 +80,11 @@ class ClientEvents(papyon.event.ClientEventInterface):
                error.response.status == 404:
                 self._client.session.add_event(Event.EVENT_BROKEN_PROFILE)
                 return
-            if error._fault == "ItemDoesNotExist": # TODO: check it's a ContentRoamingError first
-                self._client.session.add_event(Event.EVENT_BROKEN_PROFILE)
+            try:
+                if error._fault == "ItemDoesNotExist":
+                    self._client.session.add_event(Event.EVENT_BROKEN_PROFILE)
+            except AttributeError:
+                log.error("Client got an error: %s %s" % (error_type, error))
         elif error_type == papyon.event.ClientErrorType.ADDRESSBOOK:#TODO
             log.error("Client got an error handling addressbook: %s %s" % (error_type, error))
         elif error_type == papyon.event.ClientErrorType.OFFLINE_MESSAGES:#TODO
