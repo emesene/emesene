abac7dc5893a7a52a2e7debe94ab5e5a5b957a7c
jamesaxl <axlrose112@gmail.com>
2012-04-24 11:28:39 +0000

    InputBox: Improve Cycle history

--- emesene/e3/common/RingBuffer.py
+++ emesene/e3/common/RingBuffer.py
@@ -17,46 +17,33 @@
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 class RingBuffer(object):
-    def __init__(self, max=5):
+    def __init__(self, max=10):
         self.max = max
-        self.items = []
-        self.current = 0
+        self.items = ["",]
+        self.pos = 0
 
     def pop(self):
         '''remove the head of the buffer and return it'''
-        item = self.items.pop(self.current)
-
-        self.current -= 1
-
-        if self.current < 0:
-            self.current = len(self.items) - 1
-
+        item = self.items.pop(1)
         return item
 
     def push(self, item):
         '''add an element to the buffer and set it as the head of
         the buffer'''
-        if len(self.items) < self.max:
+
+        if len(self.items) + 1 < self.max:
             self.items.append(item)
-            self.current = len(self.items) - 1
-        elif self.current == self.max - 1:
-            self.items[0] = item
-            self.current = 0
-        else:
-            self.items[self.current + 1] = item
-            self.current += 1
+        elif len(self.items) - 1 < self.max:
+            self.items.append(item)
+            self.pop()
 
     def peak(self, offset=0):
         '''return the item that is in head + offset, doesn't remove it'''
-        if len(self.items) == 0:
+        if len(self.items) == 1:
             raise IndexError("peaking an empty RingBuffer")
-
-        pos = self.current + offset
-
-        if pos < 0 or pos > len(self.items) - 1:
-            pos = pos % len(self.items)
-
-        return self.items[pos]
+            
+        self.pos = offset
+        return self.items[self.pos]
 
     def __len__(self):
         '''return the current size of the buffer'''
