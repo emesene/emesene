cf821bbcc0d041ce19b7ee605552b33adde8a4d0
lfottaviano <lfottaviano@gmail.com>
2012-05-06 01:13:15 -0300

    gtkui.TextBox: Fixes   ValueError: chr() arg not in range(256) I found this when CTRL+BACKSPACE when I wanted to remove complete words.

--- emesene/gui/gtkui/TextBox.py
+++ emesene/gui/gtkui/TextBox.py
@@ -262,13 +262,13 @@ class InputText(TextBox):
         self.apply_tag()
 
         if event.state == gtk.gdk.CONTROL_MASK and \
-                 chr(event.keyval) == "p" or \
+                 event.keyval == gtk.keysyms.p or \
                     event.keyval == gtk.keysyms.Up:
 
             self.on_cycle_history()
 
         elif event.state == gtk.gdk.CONTROL_MASK and \
-                chr(event.keyval) == "n" or \
+                event.keyval == gtk.keysyms.n or \
                     event.keyval == gtk.keysyms.Down:
 
             self.on_cycle_history(1)
