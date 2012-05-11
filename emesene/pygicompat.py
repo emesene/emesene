f03791a5ccbc4eb2012b4ca5483698c6c3b47235
Jose Rostagno <joserostagno@vijona.com.ar>
2012-05-11 07:48:28 -0300

    gtk3: fix SHIFT mask bug

--- emesene/pygicompat.py
+++ emesene/pygicompat.py
@@ -39,6 +39,7 @@ Pango.SCALE_SMALL = 0.8333333333333
 
 #override control_mask due to bug in pygicompat
 Gdk.CONTROL_MASK = Gdk.ModifierType.CONTROL_MASK
+Gdk.SHIFT_MASK = Gdk.ModifierType.SHIFT_MASK
 
 if not hasattr(Gdk, 'screen_width'):
     Gdk.screen_width = Gdk.Screen.width
