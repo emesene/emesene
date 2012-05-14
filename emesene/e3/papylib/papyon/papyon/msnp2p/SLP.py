2e13415364f6fea0e33e1643f3404c4335af1b07
Riccardo (C10uD) <c10ud.dev@gmail.com>
2012-05-14 17:29:04 +0200

    Partially revert 65d0f18b5a87cd976763938d283dbbf627d7d941 - That must be made conditional with TLP version

--- emesene/e3/papylib/papyon/papyon/msnp2p/SLP.py
+++ emesene/e3/papylib/papyon/papyon/msnp2p/SLP.py
@@ -377,28 +377,28 @@ class SLPTransportResponseBody(SLPMessageBody):
     @property
     def internal_ips(self):
         try:
-            return self.get_header("IPv4Internal-Addrs").split()
+            return self.get_header("IPv4Internal-Addrs"[::-1])[::-1].split()
         except (KeyError, ValueError):
             return []
 
     @property
     def internal_port(self):
         try:
-            return int(self.get_header("IPv4Internal-Port"))
+            return int(self.get_header("IPv4Internal-Port"[::-1])[::-1])
         except (KeyError, ValueError):
             return 0
 
     @property
     def external_ips(self):
         try:
-            return self.get_header("IPv4External-Addrs").split()
+            return self.get_header("IPv4External-Addrs"[::-1])[::-1].split()
         except (KeyError, ValueError):
             return []
 
     @property
     def external_port(self):
         try:
-            return int(self.get_header("IPv4External-Port"))
+            return int(self.get_header("IPv4External-Port"[::-1])[::-1])
         except (KeyError, ValueError):
             return 0
 
