f358f2f8677443ebb1478a8778e8f27a6865d9c1
Riccardo (C10uD) <c10ud.dev@gmail.com>
2012-06-29 20:53:36 +0200

    gtk/login: add unsubscribe method in base class, fixes #1389

--- emesene/gui/gtkui/Login.py
+++ emesene/gui/gtkui/Login.py
@@ -246,6 +246,10 @@ class LoginBaseUI(gtk.Alignment):
         self.add(vbox)
         vbox.show_all()
 
+    def unsubscribe_signals(self):
+        ''' overload this if needed '''        
+        pass
+
     def _on_cancel_clicked(self, button):
         '''
         overload this
