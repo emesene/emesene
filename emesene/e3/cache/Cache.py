9bcd36a58978a27305ba3b97cf7f6ca876256193
Riccardo (C10uD) <c10ud.dev@gmail.com>
2012-12-20 21:31:10 +0100

    e3/cache: import os, closes #1566

--- emesene/e3/cache/Cache.py
+++ emesene/e3/cache/Cache.py
@@ -15,7 +15,8 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with emesene; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USAimport os
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+import os
 import abc
 import hashlib
 import subprocess
